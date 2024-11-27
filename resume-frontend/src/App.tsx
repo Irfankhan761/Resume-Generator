d0cad8c070d87ca5aa5925f90dcba502c4104eef
Irfankhan761 <irfandk1994@gmail.com>
2024-11-27 23:52:37 +0500

    Update App.tsx

--- resume-frontend/src/App.tsx
+++ resume-frontend/src/App.tsx
@@ -6,7 +6,6 @@ import { Route, Routes } from "react-router-dom";
 import { CV } from "./pages/CVBuilderPage/components/CV";
 import LoginPage from "./pages/AuthPage/Login/Login";
 import SignupPage from "./pages/AuthPage/Sigup/SignUp";
-import AuthPage from "./pages/AuthPage/Login/Login";
 
 function App() {
   return (
@@ -14,8 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/create-cv" element={<CV />} />
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-        <Route path="/login" element={<AuthPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </>
